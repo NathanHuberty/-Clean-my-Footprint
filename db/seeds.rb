308121e556fb13b2c34753bb54a07f4cd43baba6
Reka Kulinyi <reekaa85@gmail.com>
2017-11-21 17:58:35 +0100

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,41 +12,83 @@ Transportation.destroy_all
 User.destroy_all
 
 
-puts "Creating stuff"
-nathan = User.create!(first_name: "A", last_name: "B", email: "nathan@clean.com", password: "12345678")
-reka = User.create!(first_name: "A", last_name: "B", email: "reka@clean.com", password: "12345678")
-louis = User.create!(first_name: "A", last_name: "B", email: "louis@clean.com", password: "12345678")
 
-puts "User created"
+puts "Creating seed"
 
-voiture = Transportation.create!(category: "Voiture", emission: 1)
-avion = Transportation.create!(category: "Avion", emission: 2)
-train = Transportation.create!(category: "Train", emission: 3)
+user_attributes = [
+  { first_name: "A", last_name: "B", email: "nathan@clean.com", password: "12345678" },
+  { first_name: "A", last_name: "B", email: "reka@clean.com", password: "12345678" },
+  { first_name: "A", last_name: "B", email: "louis@clean.com", password: "12345678" }
+]
+
+user_attributes.each do |attr|
+  user = User.new(attr)
+  user.save!
+end
+
+# creating users with radom attributes
+8.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "12345678"
+  )
+  user.save!
+end
+
+puts "Users created"
+
+
+# creating means of transportation
+transportation_attributes = [
+  { category: "Voiture", emission: 0.1181 },
+  { category: "Avion", emission: 0.115 },
+  { category: "Train", emission: 0.0602 }
+]
+
+transportation_attributes.each do |attr|
+  transport = Transportation.new(attr)
+  transport.save!
+end
 
 puts "Transportations created"
 
-burundi = Project.create!(name: "Burundi", carbon: 1, description: "blabla")
-amazone = Project.create!(name: "Amazonia", carbon: 2, description: "blabla")
-cambodge = Project.create!(name: "Cambodgia", carbon: 3, description: "blabla")
+
+# creating projects
+project_attributes= [
+  { name: Faker::Address.country, carbon: 1, description: Faker::Lorem.paragraph },
+  { name: Faker::Address.country, carbon: 2, description: Faker::Lorem.paragraph },
+  { name: Faker::Address.country, carbon: 3, description: Faker::Lorem.paragraph },
+  { name: Faker::Address.country, carbon: 3, description: Faker::Lorem.paragraph },
+  { name: Faker::Address.country, carbon: 4, description: Faker::Lorem.paragraph }
+]
+
+project_attributes.each do |attr|
+  project = Project.new(attr)
+  project.save!
+end
 
 puts "Projects created"
 
-trip_reka = Trip.new(user: reka, transportation: avion, km: 2000, number: 1)
-compensation_reka = Compensation.create!(project: amazone, amount: 2000)
-trip_reka.compensation = compensation_reka
-trip_reka.save!
-trip_reka2 = Trip.new(user: reka, transportation: train, km: 200, number: 2)
-compensation_reka2 = Compensation.create!(project: burundi, amount: 1200)
-trip_reka2.compensation = compensation_reka2
-#amount: 2000
-trip_reka2.save!
 
-puts "stuff created"
+# creating trips, last 5 trips are not compensated!
+User.all.each do |user|
+  rand(1..5).times do
+    trip = Trip.new(user: user, transportation: Transportation.all.sample, km: rand(20..1500), number: rand(1..10) )
+    trip.save!
+  end
+end
 
-# trip_nathan = Trip.create!(user: nathan, transportation: voiture, km: 20, number: 10)
-# compensation_nathan = Compensation.create!(project: burundi, amount: )
+puts "Trips created"
 
-# trip_reka =
-# trip_louis =
-# trip1.compensation
+# creating compensations
+Trip.all[0..-5].each do |trip|
+  comp = Compensation.create!(project: Project.all.sample, amount: rand(10..450))
+  trip.update!(compensation: comp)
+end
+
+puts "Compensations created"
+puts "End of seed"
+
 
