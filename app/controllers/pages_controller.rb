a8a790fdab45e0bb4cb46ffec3262e9bd884bf71
Nathan Huberty <nathan.huberty@orange.fr>
2017-11-22 14:16:03 +0100

    refacto code banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,23 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
+  before_action :create_trip
   def home
   end
 
   def dashboard
 
     #trips/new
-    @trip = Trip.new
+
     #trips/index
 
     #compensations/new
 
     #projects/index
+  end
+
+  private
+
+  def create_trip
+    @trip = Trip.new
   end
 end
