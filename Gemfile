513d345ee9d30ed04b27c66d0465088cf1fc8535
Reka Kulinyi <reekaa85@gmail.com>
2017-11-22 12:40:28 +0100

    attachinary setup, photos on show page

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'simple_form'
 gem 'uglifier'
 
 gem 'faker'
-gem 'cloudinary'
+gem 'cloudinary', '1.1.7'
 gem 'attachinary', github: 'assembler/attachinary'
 gem 'jquery-fileupload-rails'
 gem 'coffee-rails'
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,17 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show]
 
   def show
+
+  end
+
+
+
+  def project_params
+    params.require(:project).permit(:name, :description, :carbon, photos: [])
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
