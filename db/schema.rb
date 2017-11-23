886a5beff49260728c3687864913ad59440f6f0c
Louis Mathe <mathe.louis@gmail.com>
2017-11-23 12:06:55 +0100

    add icons to transportation + front trips index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122102006) do
+ActiveRecord::Schema.define(version: 20171123104451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20171122102006) do
     t.float    "emission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "icon"
   end
 
   create_table "trips", force: :cascade do |t|
