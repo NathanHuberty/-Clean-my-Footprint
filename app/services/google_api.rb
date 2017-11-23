5880101c34788f6ec9e67ac3d19b2c0e819bafc1
Nathan Huberty <nathan.huberty@orange.fr>
2017-11-23 17:56:11 +0100

    url ASCII fixed

--- app/services/google_api.rb
+++ app/services/google_api.rb
@@ -5,7 +5,7 @@ class GoogleApi
   end
 
   def self.km_calcul(start_address, destination_address, transport)
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{start_address}&destination=#{destination_address}&mode=#{transport}&key=#{ENV['GOOGLE_API_KEY']}"
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{start_address.parameterize}&destination=#{destination_address.parameterize}&mode=#{transport}&key=#{ENV['GOOGLE_API_KEY']}"
     trip_serialized = open(url).read
     trip = JSON.parse(trip_serialized)
     return trip['routes'].first['legs'].first['distance']['value'] / 1000
