17a8ffa4e3fdc2764e4cb1f431b17b1ea0c2c423
Reka Kulinyi <reekaa85@gmail.com>
2017-11-24 16:39:47 +0100

    calculation of trip times and distance

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,18 +3,25 @@ require_relative '../services/google_api'
 class TripsController < ApplicationController
 
   def create
-    raise
-    params[:trip][:return]
-    params[:trip][:recurrence]
-
-    # if recurrence : number = return(1 ou 2) * @trip.km * number of trips
-    # if occasionnel: number = return.to_i(1 ou 2) * number of trips
 
 
+    if params[:trip][:recurrent] == "true"
+      starting_date = Date.parse("#{params[:trip]['date_since(1i)']}/#{params[:trip]['date_since(2i)']}/#{params[:trip]['date_since(3i)']}")
+      days_between = (Date.today - starting_date).to_i
+      frequency = params[:trip][:number_per].to_i
 
+      if params[:trip][:time_unit] == "jour"
+        single_trips = days_between * frequency
+      elsif params[:trip][:time_unit] == "semaine"
+        single_trips = (days_between / 7).floor * frequency
+      else # if mois
+        single_trips = (days_between / 30).floor * frequency
+      end
+    else
+      single_trips = params[:trip][:number_of_times].to_i
+    end
 
-
-    @trip = Trip.new(trip_params, number: )
+    @trip = Trip.new(trip_params)
     @trip.user = current_user
     case @trip.transportation.category
     when "Voiture" || "Avion" || "Moto"
@@ -22,13 +29,18 @@ class TripsController < ApplicationController
     when "Train" || "Transport en commun"
       transport = "transit"
     end
-    @trip.km = GoogleApi.km_calcul("#{@trip.start_address}", "#{@trip.destination_address}", "#{transport}")
+
+    @trip.number = single_trips * params[:trip][:num_return].to_i
+    @trip.km = GoogleApi.km_calcul("#{@trip.start_address}", "#{@trip.destination_address}", "#{transport}") * @trip.number
+
     if @trip.save!
       redirect_to dashboard_path
     else
       render params[:url]
     end
   end
+
+  private
 
   def trip_params
     params.require(:trip).permit(:start_address, :destination_address, :number, :transportation_id)
