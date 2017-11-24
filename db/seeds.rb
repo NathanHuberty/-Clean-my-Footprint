c138a9224c8f7114fcf404de208db618fbbc173c
Reka Kulinyi <reekaa85@gmail.com>
2017-11-24 11:55:17 +0100

    map for project show

--- db/seeds.rb
+++ db/seeds.rb
@@ -91,7 +91,7 @@ photo_urls_carbon = [
 project_attributes= [
   { name: "On Tree Planted", address: "Malaisie", carbon: 1, description: Faker::Lorem.paragraph(8, false, 2), photo_urls: photo_urls_one_tree, logo: "https://images-platform.99static.com/_l1aD_Pss_hM9aAvKVUoBTW2w5w=/500x500/top/smart/99designs-contests-attachments/30/30161/attachment_30161703" },
   { name: "Plant a Billion Trees", address: "Chine", carbon: 2, description: Faker::Lorem.paragraph(8, false, 2), photo_urls: photo_urls_billion_trees, logo: "http://static.bestfreewebresources.com/wp-content/uploads/2012/01/tree-logo-design-34.jpg" },
-  { name: "Trees for the Future", address: "Europe", carbon: 3, description: Faker::Lorem.paragraph(8, false, 2), photo_urls: photo_urls_future, logo: "http://treeocodeniagara.com/wp-content/uploads/2016/04/Tree-Planting-Sign.png" },
+  { name: "Trees for the Future", address: "Pologne", carbon: 3, description: Faker::Lorem.paragraph(8, false, 2), photo_urls: photo_urls_future, logo: "http://treeocodeniagara.com/wp-content/uploads/2016/04/Tree-Planting-Sign.png" },
   { name: "International Tree Foundation", address: "Tanzanie", carbon: 3, description: Faker::Lorem.paragraph(8, false, 2), photo_urls: photo_urls_international, logo: "http://gyr.fortlauderdale.gov/Home/ShowImage?id=2024&t=635657287021200000" }
 ]
 
