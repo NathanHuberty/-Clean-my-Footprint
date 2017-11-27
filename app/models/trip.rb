9ad0be3439a374feedaebdbe1f9b33dd54ceb5fa
Nathan Huberty <nathan.huberty@orange.fr>
2017-11-27 11:37:55 +0100

    migration lat long start address

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,6 @@
 class Trip < ApplicationRecord
   attr_accessor :num_return, :recurring, :occasional, :number_per, :time_unit, :date_since, :number_of_times
+
   belongs_to :user
   belongs_to :transportation
   belongs_to :compensation, optional: true
@@ -7,6 +8,8 @@ class Trip < ApplicationRecord
   validates :number, numericality: { greater_than: 0 }
   # validates :start_address, length: { minimum: 1 }
   # validates :destination_address, length: { minimum: 1 }
+  geocoded_by :start_address
+  after_validation :geocode
 
   scope :pending, -> { where(compensation_id: nil) }
   scope :clean, -> { where.not(compensation_id: nil) }
