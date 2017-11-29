8f1dc639cdc14037d2a167d1f77cc9a0af95a08c
Louis Mathe <mathe.louis@gmail.com>
2017-11-30 00:10:39 +0100

    le feu divin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
   def show
+    @carbon_quantity = current_user.trips.carbon_quantity
+    @relative_score = current_user.trips.relative_score
   end
+
+
+
+
+
 end
